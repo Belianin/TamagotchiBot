12728c503803cf8c200311b319c07bfbecceda96
dorichan <ars-longa-vita@mail.ru>
2018-11-26 08:09:22 +0500

    + if для тренировки

--- tamgochy/ToiletCommand.java
+++ tamgochy/ToiletCommand.java
@@ -1,4 +1,4 @@
-package tamgochy;
+//package tamgochy;
 
 public class ToiletCommand extends Command {	
 	public ToiletCommand() {
@@ -8,7 +8,12 @@ public class ToiletCommand extends Command {
 
 	@Override
 	public Reply reply(String input, String id, Bot bot) {
-		bot.getTamagochyMap().get(id).addToilet(100);
-    	return defaultReply;
+		if (bot.getTamagochyMap().get(id).canToilet) {
+			bot.getTamagochyMap().get(id).addToilet(100);
+			return defaultReply;
+		}
+		else {
+			return new Reply("Эта команда еще не открыта");
+		}
 	}
 }
