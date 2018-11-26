66b70b9959958b54995beb1c1a9df1d5d6137783
dorichan <ars-longa-vita@mail.ru>
2018-11-26 08:07:38 +0500

    + поля для тренировки

--- tamgochy/Pet.java
+++ tamgochy/Pet.java
@@ -1,17 +1,27 @@
-package tamgochy;
+//package tamgochy;
 import java.util.ArrayList;
 import java.lang.reflect.Field;
 import java.util.Date;
 import java.util.HashMap;
 //import com.sun.javafx.collections.MappingChange.Map;
 public class Pet {
+    public StringBuilder messages = new StringBuilder();
+    public int trainLvl = 0;
     public boolean alive = true;
+    public boolean train = true;
     private Date lastUpdate = new Date();
     private int hunger = 100;
     private int fun = 100;
     private int clean = 100;
     private int sleep = 100;
     private int toilet = 100;
+
+    public boolean canToilet = false;
+    public boolean canClean = false;
+    public boolean canFun = false;
+    public boolean canEat = false;
+    public boolean canSleep = false;
+
     
     public int getToilet() {
     	return toilet;
