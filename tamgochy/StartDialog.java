1f8b8894919c774aac0cbb1fb87ee3937030d110
dorichan <ars-longa-vita@mail.ru>
2018-12-02 19:43:53 +0500

    + возможность выбрать обучение

---
+++ tamgochy/StartDialog.java
@@ -0,0 +1,28 @@
+//package tamagotchi;
+
+import java.util.Date;
+
+public class StartDialog implements Dialog {
+
+	public Reply reply(UserData user, String input)
+	{
+		//наверное нужно вынести реплаи в переменные, чтобы не создавать их заново постоянно
+		if (input.toLowerCase().equals("да"))
+			return new Reply("Добро пожаловать в обучение! Я - твой питомец, и у меня есть потребности! " +
+					"Готов за мной ухаживать?", DialogName.Train, new Button[] { new Button("Да")});
+		if (!input.toLowerCase().equals("нет")) {
+			Reply reply = new Reply("Привет, это стартовый экран. Хочешь пройти обучение?");
+			reply.addRow(new Button[] { new Button("Да"), new Button("Нет")});
+			return reply;
+		}
+
+		user.events.add(new DeathEvent(user.pet));
+		return new Reply("Игра началась!", DialogName.Main, Button.getNeedsButtons());
+
+
+
+
+
+
+	}
+}
