8cde1c282c3f20bdf8a550f3861ba52fcce26b81
dorichan <ars-longa-vita@mail.ru>
2018-12-02 19:43:21 +0500

    вместо FunCommand

---
+++ tamgochy/HealthCommand.java
@@ -0,0 +1,22 @@
+//package tamagotchi;
+
+import java.util.Arrays;
+
+public class HealthCommand extends Command {
+	public HealthCommand() {
+		synonyms.addAll(Arrays.asList(new String[] {"здоровье", "жизни", "💊"}));
+
+		defaultReply = new Reply("Лечу свои раны...");
+	}
+
+	@Override
+	public Reply reply(UserData user, String input) {
+		if (user.pet.canFun) {
+			user.pet.addHealth(40);
+			return defaultReply;
+		}
+		else {
+			return new Reply("Эта команда еще не открыта");
+		}
+	}
+}
