54ad1cc914a4e29f8e3d8c20c77a300757b666bf
Andrey Belianin <belyaninwork@gmail.com>
2018-12-06 22:35:01 +0500

    Don't enter/leave dungeon when sleeping

--- tamagotchi/EnterDungeonCommand.java
+++ tamagotchi/EnterDungeonCommand.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 
 public class EnterDungeonCommand extends Command {
 	public EnterDungeonCommand() {
-		synonyms.addAll(Arrays.asList(new String[] {"🏔️ в подзмелье!", "🏔️", "подземелье"}));
+		synonyms.addAll(Arrays.asList(new String[] { "🏔️ в подзмелье!", "🏔️", "подземелье" }));
 	}
+
 	@Override
 	public Reply reply(UserData user, String input) {
-		user.events.add(new EncounterEvent(user.pet));
-		return new Reply("Питомец пошёл в подземелье.", Button.getNeedsButtons());
+		if (!user.pet.isSleep) {
+			user.events.add(new EncounterEvent(user.pet));
+			return new Reply("Питомец пошёл в подземелье.", Button.getNeedsButtons());
+		}
+		return new Reply("вроде в мейне должно это обрабатыватся");
 	}
 
 }
--- tamagotchi/LeaveDungeonCommand.java
+++ tamagotchi/LeaveDungeonCommand.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 
 public class LeaveDungeonCommand extends Command {
 	public LeaveDungeonCommand() {
-		synonyms.addAll(Arrays.asList(new String[] {"🚪 домой!", "🚪", "домой"}));
+		synonyms.addAll(Arrays.asList(new String[] { "🚪 домой!", "🚪", "домой" }));
 	}
+
 	@Override
 	public Reply reply(UserData user, String input) {
-		user.pet.InDungeon = false;
-		return new Reply("Питомец идёт домой.", Button.getNeedsButtons());
+		if (!user.pet.isSleep) {
+			user.pet.InDungeon = false;
+			return new Reply("Питомец идёт домой.", Button.getNeedsButtons());
+		}
+		return new Reply("вроде в мейне должно это обрабатыватся");
 	}
 
 }
